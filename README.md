829fa90e924f90471282f8ca9416042354d81785
Adam Wadsworth <adamkwadsworth@Adams-MBP.lan>
2020-01-08 18:18:35 +0000

    Delete Tweets using Node - Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Delete Tweets using Node
+
+```bash    
+cd delete-tweets-using-node
+npm install
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+
+const twitter = require('twitter');
+
+// https://developer.twitter.com
+
+const config = new twitter({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+
+const screen_name = 'akw_88';
+const count = 200;
+
+config.get('statuses/user_timeline', {screen_name: screen_name, count: count}, (error, tweets, response) => {
+
+  if (!error) {
+    tweets.forEach((data) => {
+
+      const tweetID = data.id_str;
+      config.post(`statuses/destroy/${tweetID}`, (error, tweet, response) => {
+
+        if (!error) {
+          console.info(tweet);
+        } else {
+          console.error(error);
+        }
+
+      });
+    });
+  } else {
+    console.error(error);
+  }
+
+});
